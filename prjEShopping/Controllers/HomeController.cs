e8e9784b65127e42c32d8c8a2f55503c725db382
yungshan7524 <yungshan75424@gmail.com>
2019-08-18 11:39:36 +0800

    no message

--- prjEShopping/Controllers/HomeController.cs
+++ prjEShopping/Controllers/HomeController.cs
@@ -20,35 +20,7 @@ namespace prjEShopping.Controllers
             var ProductList = db.tProduct.OrderByDescending(m => m.fId).Take(8).ToList();
             return View(ProductList);
         }
-        public ActionResult Register()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult Register(tMember NewMember)
-        {
-            string Uid = NewMember.fUId;
-            var member = db.tMember.Where(m => m.fUId == Uid).FirstOrDefault();
-            if (member !=null)
-            {
-                ViewBag.Msg_Fail = "帳號已有人使用，請輸入另一個!";
-            }
-            else
-            {
-                string Password = NewMember.fPwd;
-                string salt = Guid.NewGuid().ToString();
-                byte[] passwordAndSaltBytes = System.Text.Encoding.UTF8.GetBytes(Password + salt);
-                byte[] hashBytes = new System.Security.Cryptography.SHA256Managed().ComputeHash(passwordAndSaltBytes);
-                string hashString = Convert.ToBase64String(hashBytes);
 
-                NewMember.fPwd = hashString;
-                NewMember.salt = salt;
-                db.tMember.Add(NewMember);
-                db.SaveChanges();
-                ViewBag.Msg_OK = "註冊成功，請登入系統購物";
-            }
-            return View();
-        }
         public ActionResult Login()
         {
             return View();
@@ -85,37 +57,7 @@ namespace prjEShopping.Controllers
             }
             return View();
         }
-        [Authorize]
-        // GET: Member
-        public ActionResult MemberEdit()
-        {
-            
-            string uid = User.Identity.Name;
 
-            var member = db.tMember.Where(m => m.fUId == uid).FirstOrDefault();
-            return View(member);
-        }
-        [Authorize]
-        [HttpPost]
-        public ActionResult MemberEdit(tMember vMember)
-        {
-
-            string uid = User.Identity.Name;
-            var member = db.tMember.Where(m => m.fUId == uid).FirstOrDefault();
-            member.fName = vMember.fName;
-            //member.fPwd = vMember.fPwd;
-            string Password = vMember.fPwd;
-            string salt = member.salt;
-            string dbPassword = member.fPwd;
-            byte[] passwordAndSaltBytes = System.Text.Encoding.UTF8.GetBytes(Password + salt);
-            byte[] hashBytes = new System.Security.Cryptography.SHA256Managed().ComputeHash(passwordAndSaltBytes);
-            string hashString = Convert.ToBase64String(hashBytes);
-            member.fPwd = hashString;
-            member.fEmail = vMember.fEmail;
-            db.SaveChanges();
-            ViewBag.Msg = "會員基本資訊修改完成！";
-            return View();
-        }
 
     }
 }
--- prjEShopping/Controllers/MemberController.cs
+++ prjEShopping/Controllers/MemberController.cs
@@ -14,21 +14,69 @@ namespace prjEShopping.Controllers
 
         tEmployeeEntities db = new tEmployeeEntities();
         // GET: Home
-        
-        public ActionResult Index()
+        public ActionResult Register()
         {
-            string uid = User.Identity.Name;
-            if (uid != null)
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(tMember NewMember)
+        {
+            string Uid = NewMember.fUId;
+            var member = db.tMember.Where(m => m.fUId == Uid).FirstOrDefault();
+            if (member != null)
             {
-                ViewBag.memberislogin = 1;
+                ViewBag.Msg_Fail = "帳號已有人使用，請輸入另一個!";
             }
             else
             {
-                ViewBag.memberislogin = 0;
+                string Password = NewMember.fPwd;
+                string salt = Guid.NewGuid().ToString();
+                byte[] passwordAndSaltBytes = System.Text.Encoding.UTF8.GetBytes(Password + salt);
+                byte[] hashBytes = new System.Security.Cryptography.SHA256Managed().ComputeHash(passwordAndSaltBytes);
+                string hashString = Convert.ToBase64String(hashBytes);
+
+                NewMember.fPwd = hashString;
+                NewMember.salt = salt;
+                db.tMember.Add(NewMember);
+                db.SaveChanges();
+                ViewBag.Msg_OK = "註冊成功，請登入系統購物";
             }
-            var ProductList = db.tProduct.OrderByDescending(m => m.fId).Take(8).ToList();
-            return View(ProductList);
+            return View();
         }
-        
+
+        [Authorize]
+        // GET: Member
+        public ActionResult MemberEdit()
+        {
+
+            string uid = User.Identity.Name;
+
+            var member = db.tMember.Where(m => m.fUId == uid).FirstOrDefault();
+            member.fPwd = string.Empty;
+            return View(member);
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult MemberEdit(tMember vMember)
+        {
+
+            string uid = User.Identity.Name;
+            var member = db.tMember.Where(m => m.fUId == uid).FirstOrDefault();
+            member.fName = vMember.fName;
+            //member.fPwd = vMember.fPwd;
+            string Password = vMember.fPwd;
+            string salt = member.salt;
+            string dbPassword = member.fPwd;
+            byte[] passwordAndSaltBytes = System.Text.Encoding.UTF8.GetBytes(Password + salt);
+            byte[] hashBytes = new System.Security.Cryptography.SHA256Managed().ComputeHash(passwordAndSaltBytes);
+            string hashString = Convert.ToBase64String(hashBytes);
+            member.fPwd = hashString;
+            member.fEmail = vMember.fEmail;
+            db.SaveChanges();
+            ViewBag.Msg = "會員基本資訊修改完成！";
+            return View();
+        }
+
     }
+
 }
